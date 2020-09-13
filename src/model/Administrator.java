7534287ccbdd93b7646c86285370a1a8c4a618ec
Martin Chiquillo <martinschiquilloc@gmail.com>
2020-09-13 18:55:19 -0500

    Update

--- src/model/Administrator.java
+++ src/model/Administrator.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class Administrator extends Person {
 
-    
+
 
     public Administrator(int age, String name, Date dateBirth, String id) {
         super(age, name, dateBirth, id);
