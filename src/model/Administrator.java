caffd792a156827e56dcaeccb82645014e790a5a
Martin Chiquillo <martinschiquilloc@gmail.com>
2020-09-25 23:23:06 -0500

    Inserto metodo respectoa julian

--- src/model/Administrator.java
+++ src/model/Administrator.java
@@ -9,6 +9,9 @@ public class Administrator extends Person {
     public Administrator(int age, String name, Date dateBirth, String id) {
         super(age, name, dateBirth, id);
     }
+    public void showMensaje(){
+        System.out.println("Hola mis perras setenta tripple hijuepputa el juli+an");
+    }
 
 
 
