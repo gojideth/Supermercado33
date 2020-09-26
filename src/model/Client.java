51cc11951a32eba0e08fac59804ea49683df31f9
Martin Chiquillo <martinschiquilloc@gmail.com>
2020-09-25 23:16:51 -0500

    Cambios que ni puta idea

--- src/model/Client.java
+++ src/model/Client.java
@@ -3,6 +3,7 @@ package model;
 import java.util.Date;
 
 public class Client extends Person{
+
     private Charge charge;
 
     public Client(int age, String name, Date dateBirth, String id, Charge charge) {
