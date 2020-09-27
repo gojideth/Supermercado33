3bb2733c1c42be1731832a4543fbed784ee94665
julian <julian.ardila@uptc.edu.co>
2020-09-26 19:28:22 -0500

    metodos de filtrado de Market implementados en client

--- src/model/supermarket/Client.java
+++ src/model/supermarket/Client.java
@@ -6,12 +6,41 @@ import java.util.List;
 
 public class Client extends Person{
     private Charge charge;
+    private Market market;
     private Bill bill;
 
     public Client(String name, Date dateBirth, String id, Charge charge, Bill bill) {
         super(name, dateBirth, id);
         this.charge=charge;
         this.bill=bill;
+    }
+
+    public void orderAlphabet(){
+        market.orderAlphabet();
+    }
+
+    public void filterLowerPrice(double price, String typeProduct){
+        market.filterLowerPrice(price, typeProduct);
+    }
+
+    public void filterLowerPrice(double price){
+        market.filterLowerPrice(price);
+    }
+
+    public void filterMaxPrice(double maxPrice){
+        market.filterMaxPrice(maxPrice);
+    }
+
+    public void filterMaxPrice(double maxPrice, String typeProduct){
+        market.filterMaxPrice(maxPrice,typeProduct);
+    }
+
+    public void filterName(String nameProduct){
+        market.filterName(nameProduct);
+    }
+
+    public void filterRangePrices(double minPrice, double maxPrice){
+        market.filterRangePrices(minPrice, maxPrice);
     }
 
     public void productsToBuy(Product purchase){
