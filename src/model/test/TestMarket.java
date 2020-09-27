6db1ff367509c51987e8da765ebc87a5a123a19d
julian <julian.ardila@uptc.edu.co>
2020-09-26 23:07:12 -0500

    implementacion Tests por consola en TesMarket

--- src/model/test/TestMarket.java
+++ src/model/test/TestMarket.java
@@ -5,6 +5,7 @@ import model.products.*;
 import model.supermarket.Denomination;
 import model.supermarket.Market;
 import model.supermarket.Quantity;
+import org.omg.PortableInterceptor.INACTIVE;
 
 import java.util.Scanner;
 
@@ -19,38 +20,47 @@ public class TestMarket {
         administration = new Administration(market);
     }
 
-    public void test2(){
+    public void test() {
         System.out.println("Ingrese numeros de Productos");
         int productsNumber = Integer.parseInt(scanner.nextLine());
         for (int i = 0; i < productsNumber; i++) {
-            String [] line = scanner.nextLine().split(" ");
-            switch (line[0]){
+            String[] line = scanner.nextLine().split(" ");
+            switch (line[0]) {
+                case "frutas":
+                    administration.addProduct(new EarthProducts(Double.parseDouble(line[1]), line[2], Integer.parseInt(line[3]), Double.parseDouble(line[4]), new Quantity(Integer.parseInt(line[5]), Denomination.valueOf(line[6]))));
+                    break;
+                case "licor":
+                    administration.addProduct(new Liquor(Double.parseDouble(line[1]), line[2], Integer.parseInt(line[3]), Double.parseDouble(line[4]), new Quantity(Integer.parseInt(line[5]), Denomination.valueOf(line[6])), Double.parseDouble(line[7]), Integer.parseInt(line[8])));
+                    break;
+                case "aseo":
+                    administration.addProduct(new GroomingProducts(Integer.parseInt(line[1]),line[2],Integer.parseInt(line[3]),Double.parseDouble(line[4]),new Quantity(Integer.parseInt(line[5]),Denomination.valueOf(line[6]))));
+                    break;
+                case "electrodomestico":
+                    administration.addProduct(new Household(Integer.parseInt(line[1]),line[2],Integer.parseInt(line[3]),Double.parseDouble(line[4]),new Quantity(Integer.parseInt(line[5]),Denomination.valueOf(line[6])), Integer.parseInt(line[7])));
+                    break;
+                case "personal":
+                    administration.addProduct(new PersonalCareProducts(Integer.parseInt(line[1]),line[2],Integer.parseInt(line[3]),Double.parseDouble(line[4]),new Quantity(Integer.parseInt(line[5]),Denomination.valueOf(line[6]))));
+                    break;
                 case "canasta":
-                    new EarthProducts(Double.parseDouble(line[1]),line[2],Integer.parseInt(line[3]),Double.parseDouble(line[4]),new Quantity(Integer.parseInt(line[5]),Denomination.valueOf(line[6])));
-                break;
+                    administration.addProduct(new Basket(Double.parseDouble(line[1]), line[2], Integer.parseInt(line[3]), Double.parseDouble(line[4]), new Quantity(Integer.parseInt(line[5]), Denomination.valueOf(line[6]))));
+                default:
             }
         }
-
+        criteria();
     }
 
-    public void test(){
-        administration.addProduct(new EarthProducts(1800,"fresas",1,1000,new Quantity(5, Denomination.UNITS)));
-        administration.addProduct(new EarthProducts(800,"papa",1,1000,new Quantity(5, Denomination.GRAMS)));
-        administration.addProduct(new Liquor(10000, "Aguardiente antioqueño", 1, 3000, new Quantity(30, Denomination.UNITS), 0.15, 40));
-        administration.addProduct(new GroomingProducts(3750,"escoba",3,3000,new Quantity(5,Denomination.UNITS)));
-        administration.addProduct(new Household(250000,"licuadora",2,200000, new Quantity(8,Denomination.UNITS),12));
-        administration.addProduct(new PersonalCareProducts(1300,"jabon de tierra",1,750,new Quantity(15,Denomination.UNITS)));
-        administration.addProduct(new Basket(3000,"pan",15,100,new Quantity(50,Denomination.UNITS)));
-
-        market.filterType("licor");
+    public void criteria(){
+        market.filterType("canasta");
         System.out.println("======filtro======");
         market.showFilters();
         System.out.println("======filtro======");
-        market.filterRangePrices(800,15000);
+        market.filterRangePrices(0,250000);
         market.showFilters();
         System.out.println("======filtro======");
-        market.filterMaxPrice(800,"frutas");
+        market.filterMaxPrice(800,"licor");
         market.showFilters();
+        System.out.println("=============================");
+        market.show();
     }
 
     public static void main(String[] args) {
